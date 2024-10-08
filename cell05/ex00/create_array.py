e14bc7b707963e233ff7264d153dd1030a438503
Seastar13579 <105530440+Seastar13579@users.noreply.github.com>
2024-10-08 16:35:51 +0700

    Add files via upload

---
+++ cell05/ex00/create_array.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+num = [23,1,4,-6,0,12,0.123232,31]
+
+print(num)
---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+a = [32, 33, 0 ,12 ,-93]
+b = 2
+
+print("Old array:",a)
+print("New array:",list(map(lambda  x:x + b, a)))
+
+
