2c22c9f4c2ba6a5c9f67a9fd1dd57f60b891b696
Seastar13579 <105530440+Seastar13579@users.noreply.github.com>
2024-10-08 11:53:05 +0700

    Add files via upload

---
+++ cell02/ex02/password.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+password = "Python is awsome"
+x = input("Enter password:")
+x = str(x)
+
+if x == password:
+    print ("ACCESS GRANTED")
+else:
+    print ("ACCESS DENIED")
