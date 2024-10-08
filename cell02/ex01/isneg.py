8193abb365d8678ebba668ead0174e9ad67164ee
Seastar13579 <105530440+Seastar13579@users.noreply.github.com>
2024-10-08 11:51:17 +0700

    Add files via upload

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+Num = input("Enter number:")
+Num = float(Num)
+
+if Num > 0:
+    print ("This number is positive.")
+elif Num == 0:
+    print ("This number is both positive and negitive")
+else:
+    print ("This number is negitive")
