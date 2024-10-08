c6f70a4e999146e99ce8115aab56ef46f9bc97eb
Seastar13579 <105530440+Seastar13579@users.noreply.github.com>
2024-10-08 13:27:59 +0700

    Delete cell03/ex00 directory

--- cell03/ex00/to25.py
+++
@@ -1,9 +0,0 @@
-Num = int(input("Enter a number less than 25:"))
-if Num < 26:
-    while Num < 26:
-        print("Inside the loop, my variable is",Num)
-        if Num == 26:
-            break
-        Num += 1
-else:
-    print("Error")
