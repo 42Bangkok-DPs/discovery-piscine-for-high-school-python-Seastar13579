847dbb0eb91955357f9e7d161e50ecc594530227
Seastar13579 <105530440+Seastar13579@users.noreply.github.com>
2024-10-08 11:50:45 +0700

    Delete cell02/ex01 directory

--- cell02/ex01/isneg.py
+++
@@ -1,9 +0,0 @@
-Num = input("Enter number:")
-Num = float(Num)
-
-if Num > 0:
-    print("This number is positive.")
-elif Num == 0:
-    print("This number is both positive and negitive")
-else:
-    print("This number is negitive")
